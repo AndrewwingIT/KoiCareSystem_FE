25c3891a551012e993585f08de6f29c0fe71e243
cuongpz <cuongshyn2003@gmail.com>
2024-10-14 18:54:59 +0700

    Update

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,27 +1,25 @@
-// src/configs/axiosConfig.js
 import axios from "axios";
-import { API_ENDPOINTS } from "@/configs/apiConfig";
-// Lấy API_BASE_URL từ file apiConfig.js
-import { API_BASE_URL } from "./apiConfig";
+const baseUrl = "https://dss-api.azurewebsites.net";
+//const baseUrl = "https://localhost:7262";
 
-// Tạo một instance của Axios
-const axiosInstance = axios.create({
-  baseURL: API_BASE_URL, // Địa chỉ API cơ bản
-  timeout: 5000, // Thời gian timeout của request
-});
+const config = {
+  baseUrl,
+  timeout: 3000000,
+};
+const api = axios.create(config);
+api.defaults.baseURL = baseUrl;
 
-// Thêm token vào header nếu cần
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+const handleBefore = (config) => {
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
+  config.headers["Authorization"] = `Bearer ${token}`;
+  console.log(token);
+  return config;
+};
+const handleError = (error) => {
+  console.log(error);
+  return;
+};
+api.interceptors.request.use(handleBefore, handleError);
+// api.interceptors.response.use(null, handleError);
 
-export default axiosInstance;
+export default api;
---
+++ src/pages/Customer/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./HomePage.scss";
+
+function HomePage() {
+  return <h1>KoiCareSystem</h1>;
+}
+export default HomePage;
