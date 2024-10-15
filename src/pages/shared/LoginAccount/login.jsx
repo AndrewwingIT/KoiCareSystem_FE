c7f1645426f2f1120b66cf58e291bc5a8d60b685
AndrewwingIT <namndse183115@fpt.edu.vn>
2024-10-16 00:34:22 +0700

    fix ui login

--- src/pages/shared/LoginAccount/login.jsx
+++ src/pages/shared/LoginAccount/login.jsx
@@ -3,22 +3,28 @@ import "./login.scss";
 
 const Login = () => {
   return (
-    <div className="login">
-      <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-      <button
-        onClick={() => {
-          /* handle forgot password */
-        }}
-      >
-        Forgot Password
-      </button>
-      <Link to="./register"> Do not have an account? Register</Link>
-    </div>
+    <>
+      <div className="background-circles">
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
+      </div>
+      <div className="login">
+        <img src="/assets/images/image.png" alt="KoiCare Logo" className="logo" />
+        <h2>Welcome back <span>KoiCare!</span></h2>
+        <h3>Login Here</h3>
+        <form>
+          <label>Email:</label>
+          <input type="email" placeholder="Your email" />
+          <label>Password:</label>
+          <input type="password" placeholder="Your password" />
+          <button type="submit">Login</button>
+        </form>
+        <p>-Or Sign in with-</p>
+        <button className="google-signin">Sign in with Google</button>
+        <p>Not a member? <Link to="./register">Register</Link></p>
+      </div>
+    </>
   );
 };
 
